2b57f9675a6e742ba0e5c7820ab2753593507cfe
Rafael Amorim <ramorim@bionexo.com>
2021-06-25 17:35:10 -0300

    teste

--- src/js/jspages/home.js
+++ src/js/jspages/home.js
@@ -1,13 +1,10 @@
 import userStore from "../user/userStore.js";
 
-export const home = (() => {
+export const home = (async () => {
     const $ = ((selector) => document.querySelector(selector));
 
-    window.addEventListener("load", async function(event) {
-        await userStore.getUserProfile();
-        console.log(userStore.getUser())
-        alert("Bemvindo " + userStore.getUser().name)
-        $("#user").textContent = userStore.getUser().name;
-    })
-
+    await userStore.getUserProfile();
+    console.log(userStore.getUser())
+    alert("Bemvindo " + userStore.getUser().name)
+    $("#user").textContent = userStore.getUser().name;
 })(); 
