918d3b071b6d8ccf1a6ec8638a60f65810f0210e
Rafael Amorim <ramorim@bionexo.com>
2021-06-25 13:20:06 -0300

    fixing register ajax

--- src/js/register/register.js
+++ src/js/register/register.js
@@ -1,5 +1,6 @@
 const registerModule = (() => {
     const $ = ((selector) => document.querySelector(selector));
+    const name = $("#inputNameRegister");
     const email = $("#inputEmailRegister");
     const password = $("#inputPasswordRegister");
     
@@ -7,46 +8,42 @@ const registerModule = (() => {
 
     function registerUser() {
         if(isValidInputs()) {
-            alert("ainda não funfakkkk")
-            //saveNewUser(getNewUserObject(email.value, password.value))
+            saveNewUser(getNewUserObject(name.value, email.value, password.value))
         }else{
             alert("Preenche direito aí, tabarel!");
         }
     }
 
     function isValidInputs() {
-        if(email.value.trim().length > 8 && password.value.trim().length > 3) return true;
+        if(email.value.trim().length > 8 && 
+            password.value.trim().length > 4 && 
+            name.value.trim().length > 4) return true;
         return false;
     }
 
-    function getNewUserObject(email, password){
+    function getNewUserObject(name, email, password){
         return {
-            "accountNumber": 0,
-            "creditCard": 0,
-            "email": "string",
-            "name": "string",
-            "nickname": "string",
-            "password": "string",
-            "salary": 0
+            "email": email,
+            "name": name,
+            "password": password
           }
     }
 
     function saveNewUser(user){
         let headers = new Headers();
-
-        headers.append('Content-Type', 'application/json');
-    console.log(JSON.stringify(user))
-        fetch("http://localhost:8085/clients", {
-            mode: 'no-cors',
+        headers.append('content-type', 'application/json');
+        const options = {
             method: 'POST',
             headers: headers,
             body: JSON.stringify(user)
-        })
+        };
+        const prodUrl = "http://ec2-3-142-222-51.us-east-2.compute.amazonaws.com:8085";
+        const devUrl = "http://localhost:8085";
+        fetch(prodUrl + "/clients", options)
         .then(response => response)
-        .then(json => console.log(json))
+        .then(json => alert("Cadastro concluído! Faça login para continuar"))
         .catch(error => console.log('Authorization failed : ' + error.message));
     }
-
 })();
 
 export default registerModule;
