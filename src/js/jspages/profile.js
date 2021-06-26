29f3ad17856c7b01c6738b6759b74f5752ef0260
Rafael Amorim <ramorim@bionexo.com>
2021-06-26 15:38:20 -0300

    imporvements on profile

--- src/js/jspages/profile.js
+++ src/js/jspages/profile.js
@@ -1,13 +1,25 @@
 import userStore from "../user/userStore.js";
 
-const profile = (async () => {
+const profile = (() => {
     const $ = ((selector) => document.querySelector(selector));
     
-    await userStore.getUserProfile();
-    console.log(userStore.getUser())
-    $("#profile-id").textContent = userStore.getUser().id;
-    $("#profile-name").textContent = userStore.getUser().name;
-    $("#profile-nickname").textContent = userStore.getUser().nickname;
-    $("#profile-email").textContent = userStore.getUser().email;
-    $("#profile-salary").textContent = userStore.getUser().salary;
+    updateProfile();
+    //initUpdateDataBtn();
+
+    async function updateProfile() {
+        await userStore.getUserProfile();
+        $("#profile-id").textContent = userStore.getUser().id;
+        $("#profile-name").textContent = userStore.getUser().name;
+        $("#profile-nickname").textContent = userStore.getUser().nickname;
+        $("#profile-email").textContent = userStore.getUser().email;
+        $("#profile-salary").textContent = userStore.getUser().salary;
+    }
+
+    function initUpdateDataBtn() {
+        $("#updateDataBtn").addEventListener("click", () => {
+            $("#profileDataList").classList.add("display-none");
+            $("#profileDataForm").classList.remove("display-none");
+        })
+    }
+
 })(); 
